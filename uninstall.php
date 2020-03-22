ec533fddd1b76f51a1ecab9dfaea02b028ac9b9c
winrid <winrid@gmail.com>
2020-03-22 13:20:50 -0700

    adding uninstall script

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+if ( ! current_user_can( 'install_plugins' ) ) {
+	exit;
+}
+
+delete_option( 'fastcomments_tenant_id' );
+delete_option( 'fastcomments_connection_token' );
+delete_option( 'fastcomments_setup' );
