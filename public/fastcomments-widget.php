402702609bac6a887c8a3d594cf145c5302e7ab9
winrid <winrid@gmail.com>
2020-09-05 22:40:31 -0700

    Removing unused file

--- public/fastcomments-widget.php
+++
@@ -1,9 +0,0 @@
-<h1>FASTCOMMENTS IS HERE</h1>
-
-<script src="https://cdn.fastcomments.com/js/embed.min.js"></script>
-<div id="fastcomments-widget"></div>
-<script>
-    window.FastCommentsUI(document.getElementById("fastcomments-widget"), {
-        tenantId: "demo" // TODO pass post id
-    });
-</script>
