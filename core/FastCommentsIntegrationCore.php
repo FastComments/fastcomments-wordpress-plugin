007ade2a56d10f9a37b844b59b5fb8f073fd97b2
winrid <winrid@gmail.com>
2022-02-13 17:59:10 -0800

    making logs warn for CI tests

--- core/FastCommentsIntegrationCore.php
+++ core/FastCommentsIntegrationCore.php
@@ -196,13 +196,13 @@ abstract class FastCommentsIntegrationCore {
     }
 
     public function commandFetchEvents($token) {
-        $this->log('debug', "BEGIN commandFetchEvents");
+        $this->log('warn', "BEGIN commandFetchEvents");
         $fromDateTime = $this->getSettingValue('fastcomments_stream_last_fetch_timestamp');
         $hasMore = true;
         $startedAt = time();
         while ($hasMore && time() - $startedAt < 30) {
             $fromDateTimeToSend = $fromDateTime ? $fromDateTime : 0;
-            $this->log('debug', "Send events command loop... Fetching events fromDateTime=[$fromDateTimeToSend]");
+            $this->log('warn', "Send events command loop... Fetching events fromDateTime=[$fromDateTimeToSend]");
             $rawIntegrationEventsResponse = $this->makeHTTPRequest('GET', "$this->baseUrl/events?token=$token&fromDateTime=$fromDateTimeToSend", null);
             $response = json_decode($rawIntegrationEventsResponse->responseBody);
             if ($response->status === 'success') {
@@ -219,7 +219,7 @@ abstract class FastCommentsIntegrationCore {
                 break;
             }
         }
-        $this->log('debug', "END commandFetchEvents");
+        $this->log('warn', "END commandFetchEvents");
     }
 
     private function canAckLock($name, $windowSeconds) {
