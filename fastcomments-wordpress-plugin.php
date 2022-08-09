593d9043773d327575493b239afac55bbba8f065
winrid <winrid@gmail.com>
2022-08-08 17:32:05 -0700

    3.12.4 - Fixing Malformed SSO message shown when SSO is enabled and nobody is logged in. The "login to comment" option is now shown as expected.

--- fastcomments-wordpress-plugin.php
+++ fastcomments-wordpress-plugin.php
@@ -13,7 +13,7 @@ if (!defined('WPINC')) {
     die;
 }
 
-$FASTCOMMENTS_VERSION = 3.123;
+$FASTCOMMENTS_VERSION = 3.124;
 
 require_once plugin_dir_path(__FILE__) . 'admin/fastcomments-admin.php';
 require_once plugin_dir_path(__FILE__) . 'public/fastcomments-public.php';
