0ad8484944788e8030ba2ed7d659e8a6cb332641
winrid <winrid@gmail.com>
2024-08-27 00:43:44 -0700

    3.16.0

--- fastcomments-wordpress-plugin.php
+++ fastcomments-wordpress-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: FastComments
 Plugin URI: https://fastcomments.com
 Description: A live, fast, privacy-focused commenting system.
-Version: 3.15.0
+Version: 3.16.0
 Author: winrid @ FastComments
 License: GPL-2.0+
 */
